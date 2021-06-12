277922ad9638dcd855011aede84fe9558ce04d29
Yorinton <ka2ki.yori@outlook.com>
2021-06-13 03:00:48 +0900

    APIGateway - Lambda統合用のレスポンス形式に変更

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
 use lambda::{handler_fn, Context};
 use serde_json::Value;
+use serde::{Deserialize, Serialize};
+use std::cmp::PartialEq; 
 
 type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    lambda::run(handler_fn(handler)).await?;
-    Ok(())
+    let response = lambda::run(handler_fn(handler)).await?;
+    Ok(response)
 }
 
-async fn handler(event: Value, _: Context) -> Result<Value, Error> {
-    Ok(event)
+async fn handler(event: Value, _: Context) -> Result<Response, Error> {
+    let response = Response::new(200, event);
+    Ok(response)
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
+struct Response {
+    // APIGatewayに返すLambdaのレスポンス形式が決まっているため、
+    // statusCodeはキャメルケースにする必要がある
+    statusCode: u32,
+    body: String,
+}
+
+#[allow(non_snake_case)]
+impl Response {
+    fn new(status_code: u32, body: Value) -> Self {
+        Response {
+            statusCode: status_code,
+            // bodyにはjsonではなくStringを指定する必要がある
+            body: body.to_string()
+        }
+    }
 }
 
 #[cfg(test)]
@@ -27,7 +50,7 @@ mod tests {
             handler(event.clone(), Context::default())
                 .await
                 .expect("expected Ok(_) value"),
-            event
+            Response::new(200, event)
         )
     }
 }
