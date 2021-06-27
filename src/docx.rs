3cfd90d6bebcac63246fb524af57b1240c6b8fda
Yorinton <ka2ki.yori@outlook.com>
2021-06-27 23:58:16 +0900

    固定データではなくDocオブジェクトからDocxを生成するよう修正 ・.add_paragraphはDocx型の所有権を奪うため、ループ処理を行う際にcloneしつつ、元のDocxを上書きする必要がある

--- src/docx.rs
+++ src/docx.rs
@@ -1,65 +1,26 @@
 pub use docx_rs::*;
 use serde::{Deserialize, Serialize};
 
-const DUMMY: &str = "こんちわ";
-
 pub fn create_docx(doc: Doc) -> Result<(), DocxError> {
     println!("{:?}", doc);
     // lambdaで一時ファイルをファイルシステムに保存する際は、
     // /tmp/配下にしないとRead-Onlyと怒られる
     let path = std::path::Path::new("/tmp/hello.docx");
     let file = std::fs::File::create(&path).unwrap();
-    Docx::new()
-        .add_paragraph(Paragraph::new().add_run(Run::new().add_text(DUMMY)).indent(
-            Some(840),
-            None,
-            None,
-            None,
-        ))
+    let mut docx = Docx::new()
+        .add_paragraph(Paragraph::new().add_run(Run::new().add_text(doc.header)).align(AlignmentType::Center))
         .add_paragraph(Paragraph::new())
-        .add_paragraph(Paragraph::new().add_run(Run::new().add_text(DUMMY)).indent(
-            Some(840),
-            Some(SpecialIndentType::FirstLine(720)),
-            None,
-            None,
-        ))
-        .add_paragraph(Paragraph::new())
-        .add_paragraph(Paragraph::new().add_run(Run::new().add_text(DUMMY)).indent(
-            Some(1560),
-            Some(SpecialIndentType::Hanging(720)),
-            None,
-            None,
-        ))
-        .add_paragraph(
-            Paragraph::new()
-                .add_run(Run::new().add_text(" World"))
-                .align(AlignmentType::Right),
-        )
-        .add_paragraph(
-            Paragraph::new()
-                .add_run(Run::new().add_text("Hello"))
-                .numbering(NumberingId::new(2), IndentLevel::new(0)),
-        )
-        .add_abstract_numbering(
-            AbstractNumbering::new(2).add_level(
-                Level::new(
-                    0,
-                    Start::new(1),
-                    NumberFormat::new("decimal"),
-                    LevelText::new("Section %1."),
-                    LevelJc::new("left"),
-                )
-                .indent(
-                    Some(720),
-                    Some(SpecialIndentType::Hanging(320)),
-                    None,
-                    None,
-                ),
-            ),
-        )
-        .add_numbering(Numbering::new(2, 2))
-        .build()
-        .pack(file)?;
+        .add_numbering(Numbering::new(2, 2));
+    for section in doc.sections.iter() {
+        docx = docx.clone().add_paragraph(Paragraph::new().add_run(Run::new().add_text(&section.title)));
+        for paragraph in section.paragraphs.iter() {
+            docx = docx.clone().add_paragraph(Paragraph::new().add_run(Run::new().add_text(&paragraph.body)));
+            for sentence in paragraph.sentences.iter() {
+                docx = docx.clone().add_paragraph(Paragraph::new().add_run(Run::new().add_text(&sentence.body)));
+            }
+        }
+    }
+    docx.build().pack(file)?;
     Ok(())
 }
 
@@ -98,10 +59,24 @@ fn test_create_docx() {
                 title: "セクションタイトル".to_string(),
                 paragraphs: vec![
                     Para {
-                        body: "テスト段落".to_string(),
+                        body: "テスト段落1".to_string(),
                         sentences: vec![
                             Sentence {
-                                body: "テスト文章".to_string()
+                                body: "テスト文章1-1".to_string()
+                            },
+                            Sentence {
+                                body: "テスト文章1-2".to_string()
+                            }
+                        ]
+                    },
+                    Para {
+                        body: "テスト段落2".to_string(),
+                        sentences: vec![
+                            Sentence {
+                                body: "テスト文章2-1".to_string()
+                            },
+                            Sentence {
+                                body: "テスト文章2-2".to_string()
                             }
                         ]
                     }
